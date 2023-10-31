ad0e23cb77eec5c629dd5490a122c83d634d83d2
jsw2840 <jis2843@naver.com>
2023-10-31 15:50:58 +0900

    blur 처리 구현

--- demo03/src/components/Exam07.js
+++ demo03/src/components/Exam07.js
@@ -9,31 +9,33 @@ const Exam07 = ()=>{
         memberPwRe:""
     });
     const [result, setResult] = useState({//검사결과
-        memberId:false,
-        memberPw:false,
-        memberPwRe:false
+        memberId:null,
+        memberPw:null,
+        memberPwRe:null
     });
     //입력데이터가 변하면 검사결과가 자동으로 계산되도록 처리
-    useEffect(()=>{
+    const checkMember = ()=>{
         //console.log("member가 변했습니다");
         //ID검사
         const idRegex = /^[a-z][a-z0-9]{7,19}$/;
-        const idMatch = idRegex.test(member.memberId);
+        const idMatch = member.memberId.length === 0 ? null : idRegex.test(member.memberId);
 
         //PW검사
         const pwRegex = /^[A-Za-z0-9!@#$]{8,16}$/;
-        const pwMatch = pwRegex.test(member.memberPw);
+        const pwMatch = member.memberPw.length === 0 ? null : pwRegex.test(member.memberPw);
 
         //PW-RE검사
-        const pwReMatch = member.memberPw.length > 0 
-                                        && member.memberPw === member.memberPwRe;
+        const pwReMatch = member.memberPwRe.length === 0 ? null : 
+                                        member.memberPw.length > 0 && member.memberPw === member.memberPwRe;
 
         setResult({
             memberId : idMatch,
             memberPw : pwMatch,
             memberPwRe : pwReMatch
         });
-    }, [member]);
+    };
+
+    //useEffect(checkMember, [member]);
 
     //객체의 상태를 한 번에 변경하는 함수를 구현
     const changeMember = (e)=>{
@@ -58,33 +60,48 @@ const Exam07 = ()=>{
                     <div className="row mt-4">
                         <div className="col">
                             <label className="form-label">아이디</label>
-                            <input type="text" name="memberId"
-                                 className={`form-control ${result.memberId ? 'is-valid':'is-invalid'}`}
-                                value={member.memberId} onChange={changeMember}/>
-                        <div className="valid-feedback">와와bb</div>
-                        <div className="invalid-feedback">우우pp</div>
+                            <input type="text" name="memberId" 
+                                    className={`
+                                        form-control 
+                                        ${result.memberId === true ? 'is-valid' : ''}
+                                        ${result.memberId === false ? 'is-invalid' : ''}
+                                    `}
+                                    value={member.memberId} onChange={changeMember}
+                                            onBlur={checkMember}/>
+                            <div className="valid-feedback">멋진 아이디입니다!</div>
+                            <div className="invalid-feedback">사용할 수 없는 아이디입니다</div>
                         </div>
                     </div>
 
                     <div className="row mt-4">
                         <div className="col">
                             <label className="form-label">비밀번호</label>
-                            <input type="password" name="memberPw"
-                                     className={`form-control ${result.memberPw ? 'is-valid' : 'is-invalid'}`}
-                                    value={member.memberPw} onChange={changeMember}/>
-                                    <div className="valid-feedback">와와bb</div>
-                        <div className="invalid-feedback">우우pp</div>
+                            <input type="password" name="memberPw" 
+                                    className={`
+                                        form-control
+                                        ${result.memberPw === true ? 'is-valid' : ''}
+                                        ${result.memberPw === false ? 'is-invalid' : ''}
+                                    `}
+                                    value={member.memberPw} onChange={changeMember}
+                                        onBlur={checkMember}/>
+                            <div className="valid-feedback">올바른 형식의 비밀번호입니다</div>
+                            <div className="invalid-feedback">비밀번호 형식이 올바르지 않습니다</div>
                         </div>
                     </div>
 
                     <div className="row mt-4">
                         <div className="col">
                             <label className="form-label">비밀번호 확인</label>
-                            <input type="password" name="memberPwRe"
-                                 className={`form-control ${result.memberPwRe ? 'is-valid' : 'is-invalid'}`}
-                                    value={member.memberPwRe} onChange={changeMember}/>
-                                    <div className="valid-feedback">와와bb</div>
-                        <div className="invalid-feedback">우우pp</div>
+                            <input type="password" name="memberPwRe" 
+                                    className={`
+                                        form-control
+                                        ${result.memberPwRe === true ? 'is-valid' : ''}
+                                        ${result.memberPwRe === false ? 'is-invalid' : ''}
+                                    `}
+                                    value={member.memberPwRe} onChange={changeMember}
+                                    onBlur={checkMember}/>
+                            <div className="valid-feedback">비밀번호가 일치합니다</div>
+                            <div className="invalid-feedback">비밀번호가 일치하지 않습니다</div>
                         </div>
                     </div>
 
@@ -92,10 +109,11 @@ const Exam07 = ()=>{
 
                     <div className="row mt-4">
                         <div className="col">
-                            <button type="button" className="btn btn-success w-100"
-                             disabled={!(result.memberId && result.memberPw
-                                         && result.memberPwRe)}>회원가입</button>
-                    
+                            <button type="button" className="btn btn-primary w-100"
+                                disabled={!(result.memberId === true && result.memberPw === true
+                                                    && result.memberPwRe === true)}>
+                                회원가입
+                            </button>
                         </div>
                     </div>
 
