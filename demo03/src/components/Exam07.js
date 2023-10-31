e40b901ec9120cfc9338aefb79891a7942bad5ef
jsw2840 <jis2843@naver.com>
2023-10-31 14:15:18 +0900

    객체 상태 변수 문제(회원가입)

--- demo03/src/components/Exam07.js
+++ demo03/src/components/Exam07.js
@@ -1,42 +1,70 @@
 import { useState } from "react";
 
-const Exam07 =()=>{
+const Exam07 = ()=>{
 
+    //객체로 상태 변수를 정의
     const [member, setMember] = useState({
-        memberId : "testuser1",
-        memberPw : "Testuser1!",
-        memberPwRe : "Testuser1!"
+        memberId:"",
+        memberPw:"",
+        memberPwRe:""
     });
 
-    const changeMember = e=>{
+    //객체의 상태를 한 번에 변경하는 함수를 구현
+    const changeMember = (e)=>{
         setMember({
             ...member,
             [e.target.name] : e.target.value
         });
     };
 
-    return(
-        <>
-        <h1>회원가입</h1>
-        <div className="row mt-3">
-        아이디
-        <input className="form-control" name="memberId" value={member.memberId} onChange={changeMember}/>
-        </div>
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-md-10 offset-md-1">
+                    
+                    {/* 점보트론 */}
+                    <div className="p-4 text-light bg-dark rounded">
+                        <h1>객체 상태 변수 문제</h1>
+                    </div>
 
-        <div className="row mt-3">
-        비밀번호
-        <input className="form-control" name="memberPw" value={member.memberPw} onChange={changeMember}/>
-        </div>
+                    <form autoComplete="off">
 
-        <div className="row mt-3">
-        비밀번호 확인
-        <input className="form-control" name="memberPwRe" value={member.memberPwRe} onChange={changeMember}/>
-        </div>
+                    <div className="row mt-4">
+                        <div className="col">
+                            <label className="form-label">아이디</label>
+                            <input type="text" name="memberId" className="form-control"
+                                    value={member.memberId} onChange={changeMember}/>
+                        </div>
+                    </div>
 
-        <div className="row text-end">
-            <button type="submit" className="btn btn-success">가입하기</button>
+                    <div className="row mt-4">
+                        <div className="col">
+                            <label className="form-label">비밀번호</label>
+                            <input type="password" name="memberPw" className="form-control"
+                                    value={member.memberPw} onChange={changeMember}/>
+                        </div>
+                    </div>
+
+                    <div className="row mt-4">
+                        <div className="col">
+                            <label className="form-label">비밀번호 확인</label>
+                            <input type="password" name="memberPwRe" className="form-control"
+                                    value={member.memberPwRe} onChange={changeMember}/>
+                        </div>
+                    </div>
+
+                    </form>
+
+                    <div className="row mt-4">
+                        <div className="col">
+                            <button type="button" className="btn btn-primary w-100">회원가입</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </div>
-        </>
-    );
+    );    
 };
+
 export default Exam07;
