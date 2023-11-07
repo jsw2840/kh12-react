c535f3b2505ae56c812ff6071bda9362599e9d35
jsw2840 <jis2843@naver.com>
2023-11-07 16:10:58 +0900

    더보기 버튼으로 무한스크롤을 구현

--- demo05/src/components/BookInfinite.js
+++ demo05/src/components/BookInfinite.js
@@ -20,19 +20,44 @@ const BookInfinite = (props)=>{
     useEffect(()=>{
         loadBook();
     },[]);
+
+    //다음페이지
+    const nextPage = () =>{
+        setPage(page+1);//페이지 1 증가
+    };
+
+    //개수가 변하면 페이지를 1로, 목록을 모두 지우고 다시 불러야 한다
+    useEffect(()=>{
+        setPage(1);
+        setBookList([]);
+        loadBook();
+    },[]);
+
     return(
         <>
         <div className="row">
             <div className="col">
-            <h1>무한 스크롤 예제</h1>
+            <h1 className="text-center">무한 스크롤 예제</h1>
             </div>
         </div>
 
+
+        <div className="row mt-4">
+            <div className="col-2 offset-10">
+                <select value={size} onChange={e=>setSize(e.target.value)}>
+                    <option value="20">20개씩 보기</option>
+                    <option value="30">30개씩 보기</option>
+                    <option value="50">50개씩 보기</option>
+                    <option value="100">100개씩 보기</option>
+                </select>
+            </div>
+        </div>
         <div className="row mt-4">
             <div className="col">
                 <table className="table">
                     <thead>
                     <tr>
+                        <th>번호</th>
                         <th>제목</th>
                         <th>저자</th>
                         <th>출판사</th>
@@ -41,6 +66,7 @@ const BookInfinite = (props)=>{
                 <tbody>
                     {bookList.map(book=>(
                         <tr key={book.bookId}>
+                            <td>{book.bookId}</td>
                             <td>{book.bookTitle}</td>
                             <td>{book.bookAuthor}</td>
                             <td>{book.bookPublisher}</td>
@@ -49,6 +75,16 @@ const BookInfinite = (props)=>{
                 </tbody>
                     </table>
             </div>
+
+            {/* {/* 더보기 버튼 */}
+            {/* <div className="row mt-2">
+                <div className="col">
+                    <button className="btn btn-primary w-100" onClick={nextPage}>
+                        {size}개 더보기
+                    </button>
+                </div>
+            </div>
+             */}
         </div>
         </>
     );
